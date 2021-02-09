37babaf538bb1085087f0fabcd766903b5f520c9
Vladimir <vladimir.suragin@gmail.com>
2021-02-09 09:31:33 +0300

    fix player rerender

--- src/game/entities/Player/Player.ts
+++ src/game/entities/Player/Player.ts
@@ -4,18 +4,21 @@ export default class Player extends Entity {
     static height = 50;
     static width = 30;
 
-    public speed = 150;
+    public speed = 300;
 
     public render() {
         const { x, y } = this.pos;
 
         this.ctx.save();
         this.ctx.fillStyle = '#fff';
+
+        this.ctx.beginPath();
         this.ctx.moveTo(x, y + Player.height);
         this.ctx.lineTo(x + Player.width / 2, y);
         this.ctx.lineTo(x + Player.width, y + Player.height);
         this.ctx.fill();
         this.ctx.closePath();
+
         this.ctx.restore();
     }
 }
