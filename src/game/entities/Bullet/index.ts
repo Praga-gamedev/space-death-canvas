6a28dead16f25246d7faae90750fe08a131fa97d
Vladimir <vladimir.suragin@gmail.com>
2021-02-13 18:05:15 +0300

    add random bullet color :)

--- src/game/entities/Bullet/index.ts
+++ src/game/entities/Bullet/index.ts
@@ -2,19 +2,21 @@ import Entity from '../Entity';
 
 export class Bullet extends Entity {
     static size = {
-        height: 8,
-        width: 3,
+        height: 25,
+        width: 5,
     };
 
     public size = { ...Bullet.size };
 
     public speed = 500;
 
+    private color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
     public render() {
         const { x, y } = this.pos;
 
         this.ctx.save();
-        this.ctx.fillStyle = '#FFF';
+        this.ctx.fillStyle = this.color;
 
         this.ctx.moveTo(x, y);
         this.ctx.fillRect(x, y, this.width, this.height);
