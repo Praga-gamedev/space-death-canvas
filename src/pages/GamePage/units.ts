5e0db0c0d9656a7871cd9a6de074ca25d49c87cf
shipko <evgeny-shipko@yandex.ru>
2021-02-13 22:14:25 +0700

    some features

--- src/pages/GamePage/units.ts
+++ src/pages/GamePage/units.ts
@@ -6,7 +6,6 @@ export const MainBlock = styled.div`
     align-items: center;
     flex-direction: column;
     justify-content: center;
-
     width: 70%;
     height: 100%;
     padding-top: 80px !important;
@@ -24,14 +23,13 @@ export const GameDisplay = styled.div`
 export const InformationBlock = styled.div`
     display: ${({ isActive }: { isActive: boolean }) =>
         isActive ? 'flex' : 'none'};
+
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
     width: inherit;
     height: inherit;
     background-color: ${colors.GrayScale_100};
-
     font-family: 'Comfortaa', sans-serif;
     color: ${colors.GrayScale_0};
 `;
