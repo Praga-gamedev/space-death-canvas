d99a94f95938f4e84d462efa1fbef4a5b9f0dec4
shipko <evgeny-shipko@yandex.ru>
2021-02-14 17:36:13 +0700

    fix styles

--- src/pages/LeaderBoard/units.ts
+++ src/pages/LeaderBoard/units.ts
@@ -1,31 +1,10 @@
 import styled from '@emotion/styled';
 import { scrollBarStyle } from '@pages/units';
-import { colors } from 'src/colors';
-
-export const MainBlock = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    width: 70%;
-    height: 100%;
-    max-width: 1000px;
-    min-width: 500px;
-    padding-top: 80px !important;
-
-    background-color: ${colors.GrayScale_50};
-    color: ${colors.GrayScale_0};
-`;
-
-export const Title = styled.h1`
-    color: ${colors.GrayScale_20};
-    margin-top: 2%;
-`;
 
 export const DataWindow = styled.div`
     height: auto;
     width: 80%;
-    max-height: 70%;
+    max-height: 60%;
     margin-top: 2%;
     border-radius: 15px;
     overflow-y: auto;
