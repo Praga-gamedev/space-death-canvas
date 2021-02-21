81d3146da804795de0db5a7aa05976dfd5e4222a
Vladimir <vladimir.suragin@gmail.com>
2021-02-21 17:11:48 +0300

    tests init

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^src/(.*)': '<rootDir>/src/$1',
+        '^@components/(.*)': '<rootDir>/src/components/$1',
+        '^@api/(.*)': '<rootDir>/src/api/$1',
+        '^@icons/(.*)': '<rootDir>/src/assets/icons/$1',
+        '^@images/(.*)': '<rootDir>/src/assets/images/$1',
+        '^@pages/(.*)': '<rootDir>/src/pages/$1',
+        '^@types/(.*)': '<rootDir>/src/types/$1',
+        '^@store/(.*)': '<rootDir>/src/store/$1',
+        '^@game/(.*)': '<rootDir>/src/game/$1',
+    },
+};
---
+++ src/components/Button/__tests__/button.spec.ts
@@ -0,0 +1,7 @@
+describe('Button component', () => {
+    describe('Render', () => {
+        test('text rendered correctly', () => {
+            expect(true).toEqual(true);
+        });
+    });
+});
