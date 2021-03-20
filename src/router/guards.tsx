13755886d27662e107b64e4ed71e775674e9399f
ovdink <ovdink@gmail.com>
2021-03-20 16:19:41 +0300

    minus console log

--- src/router/guards.tsx
+++ src/router/guards.tsx
@@ -41,8 +41,6 @@ export const NotAuthorizedRoute: FC<ProtectedRouteProps> = ({
         [isAuth, isOffline]
     );
 
-    console.log(restProps);
-
     return <ProtectedRoute {...restProps} />;
 };
 
