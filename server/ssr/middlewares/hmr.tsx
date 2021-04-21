f6ee4662ae59881c1954c09d77afe1a25e044736
shipko <evgeny-shipko@yandex.ru>
2021-04-21 16:58:33 +0700

    fix webpack imports

--- server/ssr/middlewares/hmr.tsx
+++ server/ssr/middlewares/hmr.tsx
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import devMiddleware from 'webpack-dev-middleware';
 import hotMiddleware from 'webpack-hot-middleware';
 
-import clientConfig from '../../../webpack/client.config';
+import clientConfig from '@webpack/client.config';
 
 const compiler = webpack({ ...clientConfig, mode: 'development' });
 
